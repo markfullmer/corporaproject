536e0c4c70c5040b53f60457e02eb07cf53f3497
markfullmer <mfullmer@gmail.com>
2014-04-18 13:44:45 -0500

    fixing floating table and some style redesign

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,14 @@
     <footer>
         <div id="copyright">
+            <a href="http://www.ched.gov.ph/"><img src="./img/ched-logo.png"/></a>
+            <a href="http://www.pnu.edu.ph/3ns/"><img src="./img/3ns-logo.jpg"/></a>
+            <a href="http://www.lnu.edu.ph/"><img src="./img/lnu-logo.jpg" /></a>
         <?php 
         $meta = get_all('meta','1',$db); 
         echo $meta['content']; 
         //echo '<br />Loading time: '; echo microtime(true) - $now; 
         ?>
         </div>
-        <a href="http://www.ched.gov.ph/"><img src="/img/ched-logo.png" width="100" align="left" /></a><a href="http://www.pnu.edu.ph/3ns/"><img src="/img/3ns-logo.jpg" width="100" align="left"  /></a><a href="http://www.lnu.edu.ph/"><img src="/img/lnu-logo.jpg" width="100" align="left"  /></a>
     </footer>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
