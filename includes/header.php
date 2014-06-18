3729b857424e538bee82d601a1b714d55cb6dafe
markfullmer <mfullmer@gmail.com>
2014-06-18 13:08:46 -0500

    adding Google analytics

--- includes/header.php
+++ includes/header.php
@@ -12,6 +12,17 @@
         <link rel="stylesheet" href="css/reset.css">
         <link rel="stylesheet" href="css/scss/styles.css">
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-11398409-8', 'corporaproject.org');
+  ga('send', 'pageview');
+
+	</script>
+
     </head>
         <body>
         <!--[if lt IE 7]>
