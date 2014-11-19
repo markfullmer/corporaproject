a60e6fa67655258b560109254f049d8e8b1085a6
markfullmer <mfullmer@gmail.com>
2014-11-19 10:07:26 -0600

    restricting words page

--- includes/page.php
+++ includes/page.php
@@ -14,16 +14,16 @@
             if (check_language_permission($lang,$db)) { $name .=' (<a href="./edit.php?type=text&id='.$id.'">edit</a>)'; }
         }
     }
-    if ($type == 'article' && 'id' != 'all') { 
+    if ($type == 'article' && 'id' != 'all') {
         if (check_permissions_single('7',$db)) { $name .= ' (<a href="./edit.php?type=article&id='.$id.'">edit</a>)'; }
     }
 
 
-if (isset($_REQUEST['message'])) { 
+if (isset($_REQUEST['message'])) {
     $messageid = $_REQUEST['message'];
-    $message = get_name($messageid,'messages',$db); 
+    $message = get_name($messageid,'messages',$db);
     echo '<div class="spaced-box">'.$message[$messageid]['name'].'</div>';
-    } 
+    }
 echo '<article';
 if ($id == 'all') { echo ' class="all"'; }
 echo '>';
@@ -50,7 +50,9 @@ if ($id == 'all') {
         readability_form($db);
     }
     if ($type == 'word') {
-        echo word_list_controller($db);
+        if (check_permissions($permission = array(3))) {
+            echo word_list_controller($db);
+        }
     }
     if ($type == 'semantic') {
         $domains = get_name('all','domain',$db);
@@ -63,9 +65,9 @@ if ($id == 'all') {
     if ($type == 'text') {
         $genres = get_name('all','genre',$db);
         $results = select_single_value('meta','7','content',$db);
-        if (isset($_REQUEST['language'])) { $language = $_REQUEST['language']; } 
+        if (isset($_REQUEST['language'])) { $language = $_REQUEST['language']; }
         else { $language = '24'; }
-        if ($language == 'all') { 
+        if ($language == 'all') {
             $l = get_name('all','language',$db);
             $l_keys = array_keys($l);
             $l_join = join(',',$l_keys);
@@ -100,16 +102,16 @@ if ($id == 'all') {
         echo '<input type="submit" value="Filter" name="submit" />';
         echo '</select><input type="hidden" name="id" value="all" />';
         echo '<input type="hidden" name="offset" value="'.$offsetplus.'" />';
-        
+
         if ($total_texts > 0) {
             if ($end <= 100) { $end = $view['count']; }
-            if ($offset == 0) { 
+            if ($offset == 0) {
                 echo ' Results '.$start.'-'.$end.' of '.$view['total'].' results';
             }
             else {
                 echo ' Results '.$start.'-'.$end;
             }
-            if ($remaining > 0) { 
+            if ($remaining > 0) {
                 echo ' <input type="submit" value="Next '.$remaining.' results" name="submit_inc" />'; }
             }
         echo '</form>';
@@ -128,8 +130,8 @@ if ($id == 'all') {
                     if (isset($_SESSION['permissions'])) {
                         if (check_language_permission($value['language'],$db)) { echo ' (<a href="./edit.php?type=text&id='.$value['id'].'">edit</a>)'; }
                     }
-                    echo '</td><td>'.$author.'</td><td>'.$genre.'</td><td>'.$value['readability'].'</td><td>'.$value['word_count'].'</td></tr>'; 
-                }    
+                    echo '</td><td>'.$author.'</td><td>'.$genre.'</td><td>'.$value['readability'].'</td><td>'.$value['word_count'].'</td></tr>';
+                }
             }
             echo '</table>';
         }
@@ -162,30 +164,30 @@ if ($type == 'semantic' && $id != 'all') {
     echo '</table>';
 }
 // Content display for non table views
-if (isset($data['content'])) { 
+if (isset($data['content'])) {
 $content = $data['content'];
     if ($type == 'text' && $frequent_mod == '1') {
         // // add language permission check
         $language = select_single_value('language',$data['language'],'name',$db);
         $frequent_words = get_frequent_words($data['language'],$db);
-        echo 'The highlighted words below are not among the 1,000 most frequent in '.$language.'. To manually mark them as a common word, click on the word. 
+        echo 'The highlighted words below are not among the 1,000 most frequent in '.$language.'. To manually mark them as a common word, click on the word.
         These additions can be reviewed on the <a href="edit.php?type=language&id='.$data['language'].'">'.$language.'</a> language page.<hr />';
         print highlight_frequent('edit',$data['id'],$frequent_words,$content,$db);
     }
     else {
         if ($type == 'text') { echo '<a href="?type=text&id='.$data['id'].'&frequent_mod=1">Manually tag words in this document as frequent</a><br />'; }
-        echo nl2br($content); 
+        echo nl2br($content);
     }
 }
 ?>
-    </article> 
-<?php 
+    </article>
+<?php
 if ($id != 'all' && $type != 'semantic') {
     echo '<aside>';
     if (isset($data['sentence_count'])) { echo '<div class="statistics"><h2>Text Statistics</h2><p>Sentences: '.$data['sentence_count'].'<p>'; }
-    if (isset($data['word_count'])) { echo '<p>Words: '.$data['word_count'].'<p>'; } 
-    if (isset($data['words_per_sentence'])) { echo '<p>Words per sentence: '.$data['words_per_sentence'].'</p><p class="subtext">Shorter sentences indicate simpler reading level. Very simple texts have less than 10 words per sentence. Difficult texts can have more than 20.</p>'; } 
-    if (isset($data['readability'])) { echo '<p>Grade Level: '.$data['readability'].'</p><p class="subtext">Gives approximate grade level, based on the percent of familiar words and and sentence length.</p>'; } 
+    if (isset($data['word_count'])) { echo '<p>Words: '.$data['word_count'].'<p>'; }
+    if (isset($data['words_per_sentence'])) { echo '<p>Words per sentence: '.$data['words_per_sentence'].'</p><p class="subtext">Shorter sentences indicate simpler reading level. Very simple texts have less than 10 words per sentence. Difficult texts can have more than 20.</p>'; }
+    if (isset($data['readability'])) { echo '<p>Grade Level: '.$data['readability'].'</p><p class="subtext">Gives approximate grade level, based on the percent of familiar words and and sentence length.</p>'; }
     if (isset($data['sentence_count'])) { echo '</div>'; }
     else {
     if (isset($_REQUEST['word_search'])) { $word_search = $_REQUEST['word_search']; }
@@ -206,4 +208,4 @@ if ($id != 'all' && $type != 'semantic') {
     }
     echo '</aside>';
 }
-?> 
+?>
