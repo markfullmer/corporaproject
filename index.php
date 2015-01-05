4e84fe00a7bc57d40b3dc4ce58099d673d8c3e88
markfullmer <mfullmer@gmail.com>
2015-01-05 16:50:30 -0600

    adding languagesimilarity to pages

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php session_start();
-$now = microtime(true); 
+$now = microtime(true);
 date_default_timezone_set('America/Chicago');
 include('functions/functions.php');
 include('variables/variables.php');
@@ -8,10 +8,10 @@ include('includes/nav.php');
 if (isset($_REQUEST['type'])) { $type = $_REQUEST['type']; } else { $type = 'article'; $id = '2'; }
 if (isset($_REQUEST['frequent_mod'])) { $frequent_mod = $_REQUEST['frequent_mod']; } else { $frequent_mod = false; }
 if (isset($_REQUEST['id'])) { $id = $_REQUEST['id']; }
-if ($type == 'text' && $id != 'all') { check_permissions($permission = array(3)); }	
-$individual_pages = array('semantic','readability','statistical','map','sentence');
+if ($type == 'text' && $id != 'all') { check_permissions($permission = array(3)); }
+$individual_pages = array('semantic','readability','statistical','map','sentence','languagesimilarity');
 if (!in_array($type,$individual_pages)) { $data = get_all($type,$id,$db); }
-include('includes/page.php'); 
-include('includes/footer.php'); 
+include('includes/page.php');
+include('includes/footer.php');
 ?>
-           
+
