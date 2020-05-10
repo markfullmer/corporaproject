6d972fc8d49be23f1cf1dc0bcafe42822f9916d7
mark <mfullmer@gmail.com>
2020-05-09 22:08:36 -0500

    Add metadata

--- includes/footer.php
+++ includes/footer.php
@@ -12,7 +12,7 @@
         <?php 
         $meta = get_all('meta','1',$db); 
         echo $meta['content']; 
-        echo '<br />Loading time: '; echo microtime(true) - $now; 
+        echo '<br /><a href="https://markfullmer.com">Mark Fullmer</a>';
         ?>
         </div>
     </footer>
--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html class="no-js" lang="en">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title><?php $meta = get_all('meta','2',$db); echo $meta['content']; ?></title>
-        <meta name="description" content="">
+        <meta name="description" content="This is a database of various texts in 10 Filipino languages, created by Mark Fullmer.">
+        <meta property="og:title" content="3NS Corpora Project">
+        <meta property="og:image" content="https://corporaproject.org/img/website-flowchart.png">
+        <meta property="og:description" content="This is a database of various texts in 10 Filipino languages, created by Mark Fullmer." />
+        <meta property="og:url" content="https://corporaproject.org/" />
+        <meta property="og:site_name" content="The personal website of Mark Fullmer" />
+        <link rel="canonical" href="https://corporaproject.org/" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/reset.css">
         <link rel="stylesheet" href="css/scss/styles.css">
@@ -15,8 +18,7 @@
         <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/styles/default.min.css">
         <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/highlight.min.js"></script>
-
-	<script>
+<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -24,13 +26,7 @@
 
   ga('create', 'UA-11398409-8', 'corporaproject.org');
   ga('send', 'pageview');
+</script>
 
-	</script>
-
-    </head>
-        <body>
-        <!--[if lt IE 7]>
-            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-        <![endif]-->
-
-        <!-- This code is taken from http://twitter.github.com/bootstrap/examples/hero.html -->
+</head>
+<body>
