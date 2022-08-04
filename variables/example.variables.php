98835b6009bfaeda4452ffca9e0b3228ea910611
Mark Fullmer <mfullmer@gmail.com>
2022-08-04 14:00:50 -0700

    Pre-syntax changes

--- variables/example.variables.php
+++ variables/example.variables.php
@@ -1,3 +1,14 @@
-<?php 
-$db = new PDO('mysql:host=localhost;dbname=DBNAME', 'USER', 'PASSWORD');
-$db->query('SET NAMES "utf8"')->execute();
+<?php
+$dbhost = "database";
+$dbuser = "lamp";
+$dbpass = "lamp";
+$dbname = "lamp";
+
+try {
+  $db = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+  // set the PDO error mode to exception
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $db->query('SET NAMES "utf8"')->execute();
+} catch (PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
