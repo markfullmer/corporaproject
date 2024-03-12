3059ead5c24f24e790a96cbc9ac9a9b0d0619b25
markfullmer <mfullmer@gmail.com>
2024-03-12 16:57:20 +0000

    Update domain title metadata

--- public_html/includes/header.php
+++ public_html/includes/header.php
@@ -9,14 +9,13 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title><?php $meta = get_all('meta', '2', $db);
-        echo $meta['content']; ?></title>
+        <title>Corpus and Repository of Writing</title>
         <meta name="description" content="This is a database of various texts in 10 Filipino languages, created by Mark Fullmer.">
-        <meta property="og:title" content="3NS Corpora Project">
+        <meta property="og:title" content="Corpus and Repository of Writing">
         <meta property="og:image" content="https://corporaproject.org/img/website-flowchart.png">
         <meta property="og:description" content="This is a database of various texts in 10 Filipino languages, created by Mark Fullmer." />
         <meta property="og:url" content="https://corporaproject.org/" />
-        <meta property="og:site_name" content="The personal website of Mark Fullmer" />
+        <meta property="og:site_name" content="Corpus and Repository of Writing" />
         <link rel="canonical" href="https://corporaproject.org/" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/reset.css">
